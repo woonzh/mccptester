59aa6c3c3e7feafa46af481c00b237d63a53e7e3
woonzh <zhenhao1991@gmail.com>
2018-04-29 10:20:27 +0800

    t

--- mccpAPI.py
+++ mccpAPI.py
@@ -41,7 +41,7 @@ class Accounts(Resource):
     
 class Inventory(Resource):
     def get(self):
-        ctype = request.args.get("ctype" ,type = str)
+        ctype = request.args.get("ctype" ,type = str, ctype="")
         sellerid = request.args.get("sellerid" ,type = str, default="")
         purpose=request.args.get("purpose", type=str)
         imssku=request.args.get("imssku", type=str, default="")
