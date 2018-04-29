82cb21b8239f01f198e84011a86e08c090c32be6
woonzh <zhenhao1991@gmail.com>
2018-04-29 12:06:34 +0800

    t

--- test.py
+++ test.py
@@ -8,18 +8,16 @@ Created on Sun Apr 29 10:19:04 2018
 import requests
 import json
 
-url='http://mccptester.herokuapp.com/inventory'
+#url='http://mccptester.herokuapp.com/inventory'
+#
+#body={
+#    "sellerid":1,
+#    "purpose":"data"
+#        }
+#
+#response=requests.get(url, params=body)
 
-body={
-    "sellerid":1,
-    "purpose":"data"
-        }
-
-response=requests.get(url, params=body)
-
-#url='http://mccptester.herokuapp.com/accountdetails'
-#response=requests.get(url)
-
-
+url='http://mccptester.herokuapp.com/accountdetails'
+response=requests.get(url)
 
 print(response.content)
