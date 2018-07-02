87b17206003badd892569819351ed2ee89ff8334
woonzh <zhenhao1991@gmail.com>
2018-07-02 14:59:31 +0800

    t

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-django_heroku.settings(locals())
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
