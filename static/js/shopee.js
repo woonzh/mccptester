67f9e39215d148fc3254b473911cbbf775cbca44
woonzh <zhenhao1991@gmail.com>
2018-07-02 15:22:28 +0800

    t

--- static/js/shopee.js
+++ static/js/shopee.js
@@ -1,10 +1,5 @@
-var $TABLE = $('#table');
-var $BTN = $('#export-btn');
-var $EXPORT = $('#export');
-
-
 function onload(){
-    var shopid = <?php echo $_POST['shopid'] ?>;
+    var shopid = <?php echo $_POST["shopid"] ?>;
     alert(shopid); 
     document.getElementById("loading").style.display="none";
 }
