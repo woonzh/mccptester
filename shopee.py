a8a29df1b25b9ea188abc030864793b000c61fff
woonzh <zhenhao1991@gmail.com>
2018-07-02 10:20:29 +0800

    t

---
+++ shopee.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 10:19:02 2018
+
+@author: woon.zhenhao
+"""
+
--- static/js/shopee.js
+++ static/js/shopee.js
@@ -2,6 +2,11 @@ var $TABLE = $('#table');
 var $BTN = $('#export-btn');
 var $EXPORT = $('#export');
 
+
+function onload(){
+    document.getElementById("loading").style.display="none";
+}
+
 function shopeeRedirect(){
     alert("testing");
 }
