b9a6bd2472bddc89ed8f77746db964bfaf465749
woonzh <zhenhao1991@gmail.com>
2018-08-07 01:12:06 +0800

    t

--- mccpAPI.py
+++ mccpAPI.py
@@ -45,7 +45,6 @@ def delivery():
     return render_template('delivery.html')
 
 @app.route('/test', methods=['POST', 'OPTIONS'])
-@crossdomain(origin='*')
 def csvTest():
     if request.method == 'POST':
         f=request.files['data']
