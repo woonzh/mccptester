d4f45ef6598485223470bc8af6550c0b3e22927c
woonzh <zhenhao1991@gmail.com>
2018-08-06 23:03:35 +0800

    t

--- csvTester.py
+++ csvTester.py
@@ -41,6 +41,7 @@ def findErrors(file):
     reader=csv.reader(file, delimiter=",")
     
     for idx, row in enumerate(reader):
+        print(str(idx))
         for idx2, cell in enumerate(row):
             if checkStr(str(cell))==False:
                 print(str(idx)+ " " +str(idx2))
