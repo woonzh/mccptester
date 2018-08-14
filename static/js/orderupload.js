b5b6b968fb9d3ac50a5e2f2006b76c0ea43bc7f3
woonzh <zhenhao1991@gmail.com>
2018-08-14 15:21:18 +0800

    t

---
+++ static/js/orderupload.js
@@ -0,0 +1,62 @@
+function onload(){
+    document.getElementById("loading").style.display="none";
+}
+
+function checkupload(){
+    var x = document.getElementById("csv");
+    txt = "Select one or more files.";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+    }else {
+        if (x.value == "") {
+            txt += "Select one or more files.";
+        } else {
+            txt += "The files property is not supported by your browser!";
+            txt  += "<br>The path of the selected file: " + x.value; // If the browser does not support the files property, it will return the path of the selected file instead.
+        }
+    }
+    document.getElementById("result").innerHTML = txt;
+}
+
+function csvUpload(){
+    document.getElementById("loading").style.display="block";
+    url="https://mccptester.herokuapp.com/orderfile";
+    checkupload();
+    var fd = new FormData();
+    file=document.getElementById("csv").files[0];
+    fd.append('data', file);
+    apikey=document.getElementById("apikey").value;
+    fd.append('apikey', apikey);
+    document.getElementById("csv").value="";
+
+    $.ajax({
+      url: url,
+      type: 'POST',
+      processData: false,
+      contentType: false,
+      cache: false,
+      async:true,
+      data:fd,
+      success: function (data) {
+        resultDisp=document.getElementById("result2");
+        resultDisp.innerHTML="<br>"+data+"</br>";
+      },
+      error: function(jqxhr, status, exception) {
+          alert("Error.");
+          document.getElementById("loading").style.display="none";
+      }
+    });
+}
